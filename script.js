b713ea13a1838e150884af1df9968ee842e77c82
Javier <javipons@mail.com>
2019-12-16 19:02:53 -0300

    terminado nav

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $('.main-header__button').on('click', function() {
     $('.hamburger-menu').toggleClass('animate');
-    $('nav').slideToggle();
+    //$('nav').slideToggle();
+    $('nav').animate({width:'toggle'},350);
 });
+
+$(window).resize(function() {
+    // This will fire each time the window is resized:
+    if ($(window).width() >= 768) {
+        // if larger or equal
+        $('nav').show();
+    }
+}).resize();
