e7a6c775593717b8a60cd7a04ab67a8ac7cd18d8
Javier <javipons@mail.com>
2019-12-18 17:52:07 -0300

    buscador terminado

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ $('.main-header__button').on('click', function() {
     $('nav').animate({width:'toggle'},350);
 });
 
+$('.buscador__formulario').hide();
+$('.buscador__boton').on('click', function() {
+    $('.buscador__formulario').slideToggle();
+});
+
 $(window).resize(function() {
     // This will fire each time the window is resized:
     if ($(window).width() >= 768) {
         // if larger or equal
         $('nav').show();
+        $('.buscador__formulario').show();
+        $('.buscador__boton').hide();
+    } else {
+        $('.buscador__boton').show();
+        $('.buscador__formulario').hide();
     }
 }).resize();
